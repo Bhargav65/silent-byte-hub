0990d655cbdfebd6b563b08cf4240de997f8a46e
bhargav65 <bhargav15312317m@gmail.com>
2024-05-18 20:26:41 +0530

    commit 8:26

--- index.js
+++ index.js
@@ -140,7 +140,7 @@ app.get('/room',async(req,res)=>{
 
 app.post('/seconduser', async (req, res) => {
     const id = req.body.id;
-    connectToMongoDB();
+    db = client.db('chatapp').collection('chat');
     const doc = await db.findOne({ id: id });
 
     if (!doc) {
@@ -170,8 +170,8 @@ app.get('/chat',async(req,res)=>{
 })
 
 app.post('/firstuser', async(req, res) => {
-  connectToMongoDB();
-  res.redirect(`/${req.body.id}`)
+  db = client.db('chatapp').collection('chat');
+    res.redirect(`/${req.body.id}`)
     const id=req.body.id
     app.get(`/${req.body.id}`, async(req, res) => {
         await db.insertOne({ id: id, user: 1 });
